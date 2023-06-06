5d24867d5a9db03d06c50c574ce279e24c807048
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-06 10:19:40 +0200

    >

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BuildComponent } from './build/build.component';
+import { ViewComponent } from './view/view.component';
+import { PreviewComponent } from './preview/preview.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'build', component: BuildComponent },
+  { path: 'view', component: ViewComponent },
+  { path: 'preview', component: PreviewComponent },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { BuildComponent } from './build/build.component';
+import { ViewComponent } from './view/view.component';
+import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
+    UserComponent,
+    BuildComponent,
+    ViewComponent,
+    PreviewComponent
   ],
   imports: [
     BrowserModule,
